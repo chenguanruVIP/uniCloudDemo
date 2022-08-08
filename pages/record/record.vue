0f42fc2bcd35ec28ac1b5a756eb775dc7be77a85
陈冠儒 <1559531694@qq.com>
2022-08-08 18:01:00 +0800

    记录

--- pages/record/record.vue
+++ pages/record/record.vue
@@ -1,6 +1,43 @@
 <template>
-	<view>
-		
+	<view class="record">
+		<view class="date-box">
+			<view class="date-time">2022-08</view>
+			<Image class="date-icon" src="/static/date.png"></Image>
+		</view>
+		<view class="day-box">
+			<view  @click="cutArrows" class="day-header">
+				<view class="day-name">8月10日</view>
+				<Image :class="{'spread-class': spread}" class="date-icon" src="/static/right.png"></Image>
+			</view>
+			<view :style="{'height': spread ? 132*2+'rpx':'0rpx'}" class="list-box">
+				<view class="item-box">
+					<view class="record-info">
+						<Image src="/static/catering.png" class="item-icon"></Image>
+						<view class="name-price">
+							<view class="record-name">饮食</view>
+							<view class="record-price">￥10.30</view>
+						</view>
+					</view>
+					<view class="item-tools">
+						<view class="tools-edit">编辑</view>
+						<view class="tools-del">删除</view>
+					</view>
+				</view>
+				<view class="item-box">
+					<view class="record-info">
+						<Image src="/static/catering.png" class="item-icon"></Image>
+						<view class="name-price">
+							<view class="record-name">饮食</view>
+							<view class="record-price">￥10.30</view>
+						</view>
+					</view>
+					<view class="item-tools">
+						<view class="tools-edit">编辑</view>
+						<view class="tools-del">删除</view>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +45,118 @@
 	export default {
 		data() {
 			return {
-				
+				spread: false
 			}
 		},
 		methods: {
-			
+			cutArrows() {
+				this.spread = !this.spread
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+@import 'static/css/variable.scss';
+page {
+	background: #F5F8FD;
+}
+.date-box {
+	width: 690rpx;
+	margin: 20rpx auto;
+	border-radius: 8rpx;
+	padding: 20rpx;
+	box-sizing: border-box;
+	background: #ffffff;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	.date-time {
+		font-size: 32rpx;
+		color: #333333;
+	}
+	.date-icon {
+		width: 48rpx;
+		height: 48rpx;
+	}
+	
+}
+.day-box {
+	width: 690rpx;
+	margin: 30rpx auto;
+	border-radius: 8rpx;
+	overflow: hidden;
+	.day-header {
+		width: 100%;
+		height: 88rpx;
+		padding: 20rpx;
+		box-sizing: border-box;
+		background: $primary-color;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		.day-name {
+			font-size: 32rpx;
+			color: #ffffff;
+		}
+		.date-icon {
+			width: 42rpx;
+			height: 42rpx;
+			transition: all 0.5s;
+		}
+		.spread-class {
+			transform: rotate(-90deg);
+		}
+	}
+	.list-box {
+		background: #ffffff;
+		padding: 0rpx 30rpx;
+		height: 0px;
+		transition: all 0.4s cubic-bezier(0.25, 1, 0.25, 1);
+		.item-box {
+			padding: 20rpx 0px;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			border-bottom: 1px solid #DEE5F1;
+			.record-info {
+				display: flex;
+				.item-icon {
+					width: 90rpx;
+					height: 90rpx;
+					margin-right: 10rpx;
+				}
+				.name-price {
+					display: flex;
+					flex-direction: column;
+					justify-content: space-between;
+					align-items: center;
+					.record-name {
+						font-size: 28rpx;
+					}
+					.record-price {
+						font-size: 28rpx;
+					}
+				}
+			}
+			.item-tools {
+				display: flex;
+				align-items: center;
+				.tools-edit {
+					font-size: 28rpx;
+					color: #2979ff;
+					margin-right: 18rpx
+				}
+				.tools-del {
+					font-size: 28rpx;
+					color: #fa3534;
+				}
+			}
+		}
+		.item-box:last-child {
+			border-bottom: none
+		}
+	}
+}
 
 </style>
---
+++ utils/index.js
@@ -0,0 +1,146 @@
+export const typeMap = {
+	"catering": {
+		name: "餐饮",
+		src: "/static/catering.png"
+	},
+	"shopping": {
+		name: "购物",
+		src: "/static/shopping.png"
+	},
+	"daily": {
+		name: "日用",
+		src: "/static/daily.png"
+	},
+	"traffic": {
+		name: "交通",
+		src: "/static/traffic.png"
+	},
+	"vegetable": {
+		name: "蔬菜",
+		src: "/static/vegetable.png"
+	},
+	"fruits": {
+		name: "水果",
+		src: "/static/fruits.png"
+	},
+	"snacks": {
+		name: "零食",
+		src: "/static/snacks.png"
+	},
+	"exercise": {
+		name: "运动",
+		src: "/static/exercise.png"
+	},
+	"recreation": {
+		name: "娱乐",
+		src: "/static/recreation.png"
+	},
+	"message": {
+		name: "通讯",
+		src: "/static/message.png"
+	},
+	"costume": {
+		name: "服饰",
+		src: "/static/costume.png"
+	},
+	"hairdressing": {
+		name: "美容",
+		src: "/static/hairdressing.png"
+	},
+	"housing": {
+		name: "住房",
+		src: "/static/housing.png"
+	},
+	"family": {
+		name: "家庭",
+		src: "/static/family.png"
+	},
+	"contact": {
+		name: "社交",
+		src: "/static/contact.png"
+	},
+	"travel": {
+		name: "旅行",
+		src: "/static/travel.png"
+	},
+	"travel": {
+		name: "烟酒",
+		src: "/static/tobacco.png"
+	},
+	"numericalCode": {
+		name: "数码",
+		src: "/static/numericalCode.png"
+	},
+	"car": {
+		name: "汽车",
+		src: "/static/car.png"
+	},
+	"medical": {
+		name: "医疗",
+		src: "/static/medical.png"
+	},
+	"books": {
+		name: "书籍",
+		src: "/static/books.png"
+	},
+	"study": {
+		name: "学习",
+		src: "/static/study.png"
+	},
+	"pet": {
+		name: "宠物",
+		src: "/static/pet.png"
+	},
+	"gifts": {
+		name: "礼金",
+		src: "/static/gifts.png"
+	},
+	"present": {
+		name: "礼品",
+		src: "/static/present.png"
+	},
+	"work": {
+		name: "办公",
+		src: "/static/work.png"
+	},
+	"service": {
+		name: "维修",
+		src: "/static/service.png"
+	},
+	"donate": {
+		name: "捐赠",
+		src: "/static/donate.png"
+	},
+	"lottery": {
+		name: "彩票",
+		src: "/static/lottery.png"
+	},
+	"redPacket": {
+		name: "红包",
+		src: "/static/redPacket.png"
+	},
+	"expressage": {
+		name: "快递",
+		src: "/static/expressage.png"
+	},
+	"else": {
+		name: "其他",
+		src: "/static/else.png"
+	},
+	"refund": {
+		name: "还款",
+		src: "/static/refund.png"
+	},
+	"lend": {
+		name: "借出",
+		src: "/static/lend.png"
+	},
+	"drink": {
+		name: "饮品",
+		src: "/static/drink.png"
+	},
+	"game": {
+		name: "游戏",
+		src: "/static/game.png"
+	},
+}
